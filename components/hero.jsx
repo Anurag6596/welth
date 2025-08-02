d3467680f7e6a2ee61d1f9400294c960a79f33fb
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-08-03 02:15:31 +0530

    crating the basic Ui for the Hero sectiom

---
+++ components/hero.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+
+const HeroSection = () => {
+  return (
+    <div className="pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text">
+          Manage Your Finances <br /> with Intelligence
+        </h1>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+          Welth is a personal finance management app that helps you track your
+          expenses, set budgets, and achieve your financial goals.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8 ">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="">
+          <div>
+            <Image
+              src="/banner.jpeg"
+              alt="Hero Image"
+              width={1280}
+              height={720}
+              className=" rounded-lg shadow-2xl mx-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
