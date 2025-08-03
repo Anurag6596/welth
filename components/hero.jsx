562c1006518869945b28a43046e4fa891d202133
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-08-03 15:15:03 +0530

    completed with the landing page

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,33 @@
+"use client";
 import Link from "next/link";
-import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 
 const HeroSection = () => {
+
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <div className="pb-20 px-4">
       <div className="container mx-auto text-center">
@@ -27,8 +50,8 @@ const HeroSection = () => {
             </Button>
           </Link>
         </div>
-        <div className="">
-          <div>
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
             <Image
               src="/banner.jpeg"
               alt="Hero Image"
